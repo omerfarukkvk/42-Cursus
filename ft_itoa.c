05673e58667f227ee927ef8b4b15c49a099ec6f1
omerfarukkvk <kvkomerfaruk@gmail.com>
2022-02-10 05:42:53 +0300

    libft

--- ft_itoa.c
+++ ft_itoa.c
@@ -14,5 +14,33 @@
 
 char	*ft_itoa(int n)
 {
-	
+	char	*str;
+	int		tmp;
+	int		index;
+
+	index = 1;
+	tmp = n;
+	while (tmp && index++)
+		tmp /= 10;
+	str = (char *)malloc(sizeof(char) *((n < 0) + index + (n == 0)));
+	if (!str)
+		return (NULL);
+	str += (n < 0) + index - 1 + (n == 0);
+	*str = '\0';
+	if (n == 0)
+		*(--str) = '0';
+	index = (n >= 0) * 2 - 1;
+	while (n)
+	{
+		*(--str) = (n % (10 * index)) * index + '0';
+		n /= 10;
+	}
+	if (index < 0)
+		*(--str) = '-';
+	return (str);
 }
+
+/*int main()
+{
+    printf("%s", ft_itoa(-68768));
+}*/
