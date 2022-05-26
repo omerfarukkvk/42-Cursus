7c56763c789e52824789b13720ac9e2ba5c13879
Omer faruk Kavak <okavak@k1m09s06.42istanbul.com.tr>
2022-05-26 19:22:45 +0300

    some func added

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -5,14 +5,60 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: okavak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 15:09:58 by okavak            #+#    #+#             */
-/*   Updated: 2022/03/22 19:33:37 by okavak           ###   ########.fr       */
+/*   Created: 2022/05/26 13:08:10 by okavak            #+#    #+#             */
+/*   Updated: 2022/05/26 19:09:06 by okavak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
+int	ft_putchar(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+int	ft_check(va_list args,char letter)
+{
+	int	length;
+
+	length = 0;
+	if (letter == 'c')
+		length += ft_putchar(va_arg(args,int));
+	else if (letter == 's')
+		length += ft_putstr(va_arg(args,char *));
+	/*else if (letter == 'p')
+	else if (letter == 'd')
+	else if (letter == 'i')
+	else if (letter == 'u')
+	else if (letter == 'x')
+	else if (letter == 'X')
+	else if (letter == '%')*/
+	return (length);
+}
 int	ft_printf(const char *str, ...)
 {
-
+	va_list	args;
+	int		i;
+	int		length;
+	
+	i = 0;
+	length = 0;
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			length += ft_check(args,str[i + 1]);
+			i++;
+		}
+		else
+			length += ft_putchar(str[i]);
+		i++;
+	}
+	va_end(args);
+	return (length);
+}
+int	main()
+{
+	printf("veri=%d\n",ft_printf("%s\n","Hello world"));
 }
--- Printf/ft_printf.h
+++ Printf/ft_printf.h
@@ -1,30 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: okavak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 19:33:21 by okavak            #+#    #+#             */
-/*   Updated: 2022/03/22 19:33:48 by okavak           ###   ########.fr       */
+/*   Created: 2022/05/26 13:11:12 by okavak            #+#    #+#             */
+/*   Updated: 2022/05/26 19:09:27 by okavak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-# include <stdio.h>
 # include <stdarg.h>
 # include <unistd.h>
-# include <stdlib.h>
+# include <stdio.h>
 
-void	ft_putstr(char *str, int *res_val);
-void	ft_putchar(char str, int *res_val);
-void	ft_putnbr(int nb, int *res_val);
-char	*ft_convert_base(size_t number, char *base_str);
-void	ft_p_str(unsigned long number, int *res_val);
-void	ft_put_u(unsigned int number, int *res_val);
-size_t	ft_strlen(const char *str);
-int		ft_printf(const char *str, ...);
+int			ft_printf(const char *str, ...);
+int			ft_check(va_list args,char letter);
+int			ft_putchar(int c);
+int			ft_putstr(char *s);
 
 #endif
