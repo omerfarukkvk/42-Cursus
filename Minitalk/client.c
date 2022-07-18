ec8146d659d6fd737bceb3028f79b2f002b8070e
Ömer Faruk Kavak <kvkomerfaruk@gmail.com>
2022-07-18 14:25:32 +0300

    norm rules completed

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -1,12 +1,13 @@
 #include <signal.h>
-# include "ft_printf.h"
+#include "ft_printf.h"
 #include <unistd.h>
 
-void ft_ok(int signal)
+void	ft_ok(int signal)
 {
 	if (signal == SIGUSR1)
 		ft_printf("Process Successful");
 }
+
 int	ft_atoi(char *str)
 {
 	int	i;
@@ -17,38 +18,38 @@ int	ft_atoi(char *str)
 	return (i);
 }
 
-void ft_procces(int pid,char *str)
+void	ft_procces(int pid, char *str)
 {
-	int i;
-	char c;
+	int		i;
+	char	c;
 
-	i=0;
-	c=0;
+	i = 0;
+	c = 0;
 	while (str[c])
 	{
-		i=7;
-		while (i>=0)
+		i = 7;
+		while (i >= 0)
 		{
-			if(str[c] >> i & 1)
-				kill(pid,SIGUSR1);
+			if (str[c] >> i & 1)
+				kill(pid, SIGUSR1);
 			else
-				kill(pid,SIGUSR2);
+				kill(pid, SIGUSR2);
 			i--;
 			usleep(50);
 		}
 		c++;
 	}
 }
-int main(int argc,char **argv)
-{
-	int pid;
 
-	pid=ft_atoi(argv[1]);
-	//ft_printf("%d",pid);
-	if(argc != 3)
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	pid = ft_atoi(argv[1]);
+	if (argc != 3)
 	{
 		ft_printf("You failed.\n");
-		return(0);
+		return (0);
 	}
 	signal (SIGUSR1, ft_ok);
 	ft_procces(pid, argv[2]);
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -8,38 +8,37 @@ void	ft_server(int signal, siginfo_t *info, void *context)
 	static char	c = 0;
 
 	(void)context;
-	if(signal == SIGUSR1)
-		c = c | 1;
+	if (signal == SIGUSR1)
+	c = c | 1;
 	if (++i == 8)
 	{
-		i=0;
+		i = 0;
 		if (!c)
 		{
-			kill(info->si_pid,SIGUSR1);
+			kill(info->si_pid, SIGUSR1);
 			ft_printf("\n");
 		}
-		ft_printf("%c",c);
-		c=0;
+		ft_printf("%c", c);
+		c = 0;
 	}
 	else
-		c <<=1;
+		c <<= 1;
 }
 
-int main(void)
+int	main(void)
 {
 	struct sigaction	server;
-	int pid;
+	int					pid;
 
-	pid=getpid();
-	ft_printf("Server pid=%d\n",pid);
-	server.sa_flags= SA_SIGINFO;
-	server.sa_sigaction= ft_server;
-	if (sigaction(SIGUSR1,&server,0)== -1)
+	pid = getpid();
+	ft_printf("Server pid=%d\n", pid);
+	server.sa_flags = SA_SIGINFO;
+	server.sa_sigaction = ft_server;
+	if (sigaction(SIGUSR1, &server, 0) == -1)
 		ft_printf("Signal Error\n");
-	if (sigaction(SIGUSR2,&server,0)== -1)
+	if (sigaction(SIGUSR2, &server, 0) == -1)
 		ft_printf("Signal Error\n");
 	while (1)
 		pause();
-	return(0);
-
+	return (0);
 }
